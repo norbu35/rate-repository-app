dbe59623724d9a909f05ab0be95f9c2707bdeca9
Norov <norbu.erdene@gmail.com>
2022-10-23 19:49:53 +0800

    exercise 10.10 done

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
 import Main from './src/components/Main';
+import theme from './src/theme';
 
 const App = () => {
   return (
     <>
       <NativeRouter>
-        <Main />
+        <Main style={theme.fonts} />
       </NativeRouter>
       <StatusBar style="auto" />
     </>
