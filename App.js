7a29d51c94d063e0003ee18adff2491a4058b5af
Norov <norbu.erdene@gmail.com>
2022-10-25 12:20:41 +0800

    environment variables added

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client';
+import Constants from 'expo-constants';
+import { StatusBar } from 'expo-status-bar';
 import Main from './src/components/Main';
 import theme from './src/theme';
+import createApolloClient from './apolloClient';
 
+const apolloClient = createApolloClient();
 const App = () => {
   return (
     <>
       <NativeRouter>
-        <Main style={theme.fonts} />
+        <ApolloProvider client={apolloClient}>
+          <Main style={theme.fonts} />
+        </ApolloProvider>
       </NativeRouter>
       <StatusBar style="auto" />
     </>
